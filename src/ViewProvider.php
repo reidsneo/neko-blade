e8778fb66d7f4785cf5915d262196e42a6b326c0
unknown <edwinardiant@yahoo.com>
2020-03-22 19:42:40 +0800

    22mar

--- src/ViewProvider.php
+++ src/ViewProvider.php
@@ -20,10 +20,33 @@ class ViewProvider extends Provider {
 	{
 		$app = $this->app;
 		$app['blade'] = $app->container->singleton(function($container) use ($app) {
-			$view_paths = [ $app->config['app.path']."themes/".$app->config['user_theme'] ];
+			$theme="";
+            foreach ($app->request->route()->getMiddlewares() as $key => $val) {
+                if (strpos($val, 'theme') !== FALSE) {
+                    $theme=explode(":",$val)[1];
+                }
+            }
+            if($theme!="")
+            {
+                $theme=$theme;
+            }else{
+                $theme=$app->config->get('user_theme');
+			}
+			
+            $view_paths = [$app->config->get('app.path')."themes/".$theme];
+			//$view_paths = [ $app->config['app.path']."themes/".$app->config['user_theme'] ];
 			$view_cache_path = $app->config['app.path']."themes/_cache";
-
 			$blade = new Blade($view_paths, $view_cache_path);
+
+			$blade->directive('uppercase', function($exp) {
+				return "<?php echo strtoupper({$exp});?>";
+			});
+
+			$blade->directive('set', function($exp) {
+				list($name, $val) = explode(',', $exp);
+				return "<?php {$name} = {$val}; ?>";
+			});
+
 			return $blade;
 		});
 	}
@@ -35,7 +58,7 @@ class ViewProvider extends Provider {
 	{
 		$app = $this->app;
 
-		$app->macro('blade', function($file, array $data = []) use ($app) {
+		$app->macro('theme', function($file, array $data = []) use ($app) {
 			return $app->blade->render($file, $data);
 		});
 	}
